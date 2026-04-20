5b4042670bb8954c353beed04a5d0723ce3a69aa
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-04-20 11:20:35 +0200

    feat(model):user model + bout de todo model

--- todo-app-mysql-1.0.7/backend/models/user.model.js
+++ todo-app-mysql-1.0.7/backend/models/user.model.js
@@ -1,29 +1,60 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    name: {
-      type: DataTypes.STRING
-    },
-    address: {
-      type: DataTypes.STRING
-    },
-    zip: {
-      type: DataTypes.INTEGER
-    },
-    location: {
-      type: DataTypes.STRING
-    }
-  });
-  return User;
-};
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+    // validate: {
+    //   isEmail: true
+    // }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  zip: {
+    type: Number
+  },
+  location: {
+    type: String
+  }
+});
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
+
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('user', {
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true,
+//       validate: {
+//         isEmail: true
+//       }
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     },
+//     name: {
+//       type: DataTypes.STRING
+//     },
+//     address: {
+//       type: DataTypes.STRING
+//     },
+//     zip: {
+//       type: DataTypes.INTEGER
+//     },
+//     location: {
+//       type: DataTypes.STRING
+//     }
+//   });
+//   return User;
+// };
